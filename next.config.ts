1b93ca5d7d7203806f0a863bd97d0acb1da9b336
Bastin <BastinJafari@gmail.com>
2025-06-16 15:18:53 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,18 @@
-export default {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
-        search: ''
       },
       {
         protocol: 'https',
         hostname: '*.public.blob.vercel-storage.com',
-        search: ''
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
+
+export default nextConfig;
